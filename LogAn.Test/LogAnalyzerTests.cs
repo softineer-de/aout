439efa696031ef8d455eb506c7032579fe8f3daf
Andreas Pietruschka <andreas.pietruschka@gmail.com>
2018-10-12 21:57:41 +0200

    Initial commit

---
+++ LogAn.Test/LogAnalyzerTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using LogAn;
+
+namespace LogAn.Test
+{
+  [TestFixture]
+  public class LogAnalyzerTests
+  {
+    [Test]
+    public void IsValidFileName_BadExtension_ReturnsFalse()
+    {
+      LogAnalyzer analyzer = new LogAnalyzer();
+
+      bool result = analyzer.IsValidLogFileName("fileWithBadExtension.foo");
+
+      Assert.False(result);
+    }
+  }
+}
---
+++ LogAn/LogAnalyzer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn
+{
+  public class LogAnalyzer
+  {
+    public bool IsValidLogFileName(string FileName)
+    {
+      if (FileName.EndsWith(".SLF"))
+      {
+        return false;
+      }
+
+      return true;
+    }
+  }
+}
